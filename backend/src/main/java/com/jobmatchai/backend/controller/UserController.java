9cef6649fbe4d95a72393184a58f3226a1af8b5b
bayan-abas <bayan.abas2003@gmail.com>
2026-05-17 17:32:05 +0300

    change
    change

---
+++ backend/src/main/java/com/jobmatchai/backend/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.jobmatchai.backend.controller;
+
+import com.jobmatchai.backend.model.User;
+import com.jobmatchai.backend.repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin(origins = "http://localhost:5173")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
